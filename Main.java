fecc42246d8fbe3adfc46dc4de3924a4225b2a43
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-09 15:19:13 +0900

    ファイル名変更

---
+++ Main.java
@@ -0,0 +1,16 @@
+// スッキリわかるJava入門
+// コメント例1(行末まで)
+/*コメント例2(複数行でも可)*/
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("すがわら");
+		System.out.println("31歳です");
+		System.out.println("お酒が好きです");
+		System.out.println("31 + 31の計算をします");
+		System.out.println(31 + 31);
+		System.out.println(35 - 10);
+		System.out.println(-5 * 2);
+		System.out.println(6 * 6 * 3.14);
+		System.out.println("答えは" + 64);
+	}
+}
--- index.java
+++
@@ -1,8 +0,0 @@
-// スッキリわかるJava入門
-public class Main {
-	public static void main(String[] args) {
-		System.out.println("すがわら");
-		System.out.println("31歳です");
-		System.out.println("お酒が好きです");
-	}
-}
