c0c59e74ad8cd081426a42de1c95b4e25fc8f57c
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-09 18:50:44 +0900

    finalをつけ上書き防止

--- Main.java
+++ Main.java
@@ -12,5 +12,23 @@ public class Main {
 		System.out.println(-5 * 2);
 		System.out.println(6 * 6 * 3.14);
 		System.out.println("答えは" + 64);
+
+		// 変数宣言
+	    int age;
+	    age = 30;
+	    System.out.println("私の年齢は"+ age + "です");
+	    // 変数の上書き
+	    age = 35;
+	    System.out.println("でも本当は" + age + "なんです");
+
+	    // finalをつけ書き換えを防止
+		final double PI = 3.14;
+		int pie = 5;
+		System.out.println("半径" + pie + "cmのパイの面積は、");
+		System.out.println(pie * pie * PI);
+		System.out.println("パイの半径を倍にします");
+		//PI = 10;
+		//System.out.println("半径" + pie + "cmのパイの面積は、");
+		//System.out.println(pie * pie * PI);
 	}
 }
