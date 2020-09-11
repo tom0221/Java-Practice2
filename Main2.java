658e0b8c3669019c290e574a89c42860050c5980
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-11 15:02:40 +0900

    Math.max(e, f);

--- Main2.java
+++ Main2.java
@@ -15,5 +15,26 @@ public class Main {
         c = 100;
         c++;
         System.out.println(c);
+
+        // 強制的な型変換(基本的に使わない)
+        int age = (int)3.2;
+        System.out.println(age);
+
+        // 文字列の連結
+        String msg = "私の年齢は" + 23;
+        System.out.println(msg);
+
+        // 改行なし出力
+        String name = "すがわら";
+
+        System.out.print("私の名前は");
+        System.out.print(name);
+        System.out.println("です");
+
+        // 大きい方の数値を出力する命令
+        int e = 5;
+        int f = 3;
+        int m = Math.max(e, f);
+        System.out.println("比較実験：" + e + "と" + f + "とで大きい方は・・・" + m);
 	}
 }
