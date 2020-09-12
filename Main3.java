7737917f27f8a24e80a143cf0e9b2a07fc43fb60
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-12 21:34:29 +0900

    あえてbreak文を書かないswitch例文

--- Main3.java
+++ Main3.java
@@ -16,5 +16,52 @@ public class Main{
 			System.out.println("ノックする");
 			System.out.println("1分待つ");
         }
+
+		// 冗長でスッキリしない見本 if-else文
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(4) + 1;
+		if (fortune == 1) {
+		    System.out.println("大吉");
+		} else if (fortune == 2) {
+		    System.out.println("中吉");
+		} else if (fortune == 3) {
+		    System.out.println("吉");
+		} else {
+		    System.out.println("凶");
+		}
+
+		// ↑をswitch文に書き直し(適切)
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(4) + 1;
+		// switchの後には変数名を書く。
+		switch (fortune) {
+			case 1:
+				System.out.println("大吉");
+				break;
+			case 2:
+				System.out.println("中吉");
+				break;
+			case 3:
+				System.out.println("吉");
+				break;
+			default:
+				System.out.println("凶");
+		}
+
+		// あえてbreak文を書かないswitch例文
+		System.out.println("あなたの運勢を占います");
+		int fortune = new java.util.Random().nextInt(5) + 1;
+		switch (fortune) {
+			case 1:
+			case 2:
+				System.out.println("いいね");
+				break;
+			case 3:
+				System.out.println("普通です");
+				break;
+			case 4:
+			case 5:
+				System.out.println("うーん・・・");
+		}
 	}
 }
