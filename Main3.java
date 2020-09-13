c39960faf2f621bc730b2281b91e9d129369ca06
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-14 00:15:19 +0900

    Main4作成と配列と変数宣言

--- Main3.java
+++ Main3.java
@@ -180,5 +180,22 @@ public class Main{
                     System.out.println("変更します");
                     break;
             }
+
+        // 練習3-6
+        System.out.println("数当てゲーム");
+        int ans = new java.util.Random().nextInt(10);
+            for (int i = 0; i < 5; i++) {
+                System.out.println("0〜9の数字を入力してください");
+                int num = new java.util.Scanner(System.in).nextInt();
+
+                if (num == ans) {
+                    System.out.println("アタリ！");
+                    break;//当たりだった場合のみ処理を中断させる！
+                } else {
+
+                } System.out.println("違います");
+            }
+
+		System.out.println("ゲームを終了します");
 	}
 }
---
+++ Main4.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		// 配列変数の宣言
+		int[] scores;
+		scores = new int[5];
+		int num = scores.length;
+		System.out.println("要素の数" + num);
+	}
+}
