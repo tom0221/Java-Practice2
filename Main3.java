07af173ec8b24f9f45ef99979dc5a0a4f2c1f410
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-13 12:38:08 +0900

    練習3-5まで

--- Main3.java
+++ Main3.java
@@ -162,5 +162,23 @@ public class Main{
 		    }
 
 		System.out.println("ごちそうさまでした");
+
+		// 練習3-5
+		System.out.print("[メニュー] 1:検索 2:登録 3:削除 4:変更>");
+        int selected = new java.util.Scanner(System.in).nextInt();
+            switch (selected) {
+                case 1:
+                    System.out.println("検索します");
+                    break;
+                case 2:
+                    System.out.println("登録します");
+                    break;
+                case 3:
+                    System.out.println("削除します");
+                    break;
+                case 4:
+                    System.out.println("変更します");
+                    break;
+            }
 	}
 }
