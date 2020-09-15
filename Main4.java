f508202a58ad62a3b13053d9c79c0ff741b8ab85
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-15 15:57:33 +0900

    [finish]練習4-4

--- Main4.java
+++ Main4.java
@@ -95,6 +95,41 @@ public class Main {
 		scores[1] [2] = 70;
 		System.out.println(scores[1] [1]);
 
+		// コード4-19 親配列と子配列の要素数表示
+		int[] [] scores = {{40, 50, 60}, {80, 60, 70}};
+		System.out.println(scores.length);
+		System.out.println(scores[0].length);
+
+
+		// 練習4-1
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean answers = new boolean[3];
+		String[] names = new String[3];
+
+		// 練習4-2
+		int[] moneyList = {121902, 8302, 55100};
+		for (int i = 0; i < moneyList.length; i++) {
+		    System.out.println(moneyList[i]);
+		    System.out.println()
+		}
+
+		for (int m : moneyList) {
+		    System.out.println(m);
+		}
+
+		// 練習4-4
+		int[] numbers = {3, 4, 9};
+		System.out.println("1桁の数字を入力してください");
+		int input = new java.util.Scanner(System.in).nextInt();
+
+		for (int n :numbers) {
+		    if (n == input) {
+		        System.out.println("アタリ");
+		    }
+		}
+
+
 
 
 	}
