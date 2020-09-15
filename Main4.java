2c7e4005921d3c4fbac0e0f54bf6170433c2e9dc
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-15 15:23:14 +0900

    2次元配列

--- Main4.java
+++ Main4.java
@@ -66,6 +66,34 @@ public class Main {
 		    }
 		}
 
+		// 配列コードの上書き
+		int[] arrayA = {1, 2, 3};
+		int[] arrayB;
+		arrayB = arrayA;
+		arrayB[0] = 100;
+		System.out.println(arrayA[0]);
+
+		// コード4-16 ガベージコレクション
+		boolean judge = true;
+		if (judge == true) {
+		    int[] array = {1, 2, 3};//配列の中身は、ブロック外から参照できないgarbage
+		    System.out.println(array[0]);
+		}
+
+		// コード4-17 nullの利用(nullを入れることで、参照しない状態に切る)
+		int[] array = {1, 2, 3};
+		array = null;
+		array[0] = 10;
+
+		// コード4-18 2次元配列
+		int[] [] scores = new int[2] [3]; //2行で3列の意味
+		scores[0] [0] = 40;
+		scores[0] [1] = 50;
+		scores[0] [2] = 60;
+		scores[1] [0] = 80;
+		scores[1] [1] = 60;
+		scores[1] [2] = 70;
+		System.out.println(scores[1] [1]);
 
 
 
