8ce470b269b70bd79f88b03a15333989dd6e0f0a
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-15 12:12:17 +0900

    DNAの記号のランダム表示

--- Main4.java
+++ Main4.java
@@ -18,7 +18,7 @@ public class Main {
 		    System.out.println(scores[i]);
 		}
 
-		// 点数評価プログラム(for文の利用)
+		// コード4-10 点数評価プログラム(for文の利用)
 		int[] = scores = {20, 30, 40, 50, 80};
 		int sum = 0;//集計結果を入れるために変数を初期化する
 		for (int i = 0; i < scores.length; i++) {
@@ -27,5 +27,47 @@ public class Main {
 		int avg = sum / scores.length;
 		System.out.println("合計点:" + sum);
 		System.out.println("平均点:" + avg);
+
+		// コード4ー11 50点以上の科目の数を調べる
+		int[] scores = {20, 30, 40, 50, 80};
+		int count = 0;
+		for (int i = 0; i < scores.length; i++) {
+		    // 条件式(条件に合致するか判定)
+		    if (scores[i] >= 50) {
+		        count++;
+		    }
+		}
+		System.out.println("50点以上の科目の数は" + count);
+
+
+		// コード4-12 DNAの記号をランダム表示
+		int[] seq = new int[10];
+
+		// 塩基配列をランダム生成
+		for (int i = 0; i < seq.length; i++) {
+		    seq[i] = new java.util.Random().nextInt(4);
+		}
+
+		// 生成した塩基配列の記号を表示
+		for (int i = 0; i < seq.length; i++) {
+		    switch (seq[i]) {
+		        case 0:
+		            System.out.println("A");
+		            break;
+		        case 1:
+		            System.out.println("T");
+		            break;
+		        case 2:
+		            System.out.println("G");
+		            break;
+		        case 3:
+		            System.out.println("C");
+		            break;
+		    }
+		}
+
+
+
+
 	}
 }
