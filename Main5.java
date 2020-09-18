080ec334dddf9f46216496fe8c930d72aaeb5e3c
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-18 12:31:21 +0900

    オーバーロード(引数の型が異なる場合)

--- Main5.java
+++ Main5.java
@@ -53,5 +53,28 @@ public class Main {
 	    System.out.println(add(10, 20), add(30, 40));
 	}
 
-	
+	// コード5-10 オーバーロード(引数の型が異なる場合)
+
+	// 1つ目のaddメソッド
+	public static int add(int x, int y) {
+	    return x + y;
+	}
+
+	// 2つ目のaddメソッド
+	public static double add(double x, double y) {
+	    return x + y;
+	}
+
+	// 3つ目のaddメソッド
+	public static String add(String x, String y) {
+	    return x + y;
+	}
+
+	public static void main(String[] args) {
+	    System.out.println(add(10, 20));          //1つ目のメソッドが呼び出される
+	    System.out.println(add(3.5, 2.7));        //2つ目のメソッドが呼び出される
+	    System.out.println(add("Hello", "World"));//3つ目のメソッドが呼び出される
+	}
+
+
 }
