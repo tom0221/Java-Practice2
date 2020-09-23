0dd5848d62d2c2e0c5b1c555dd510a4d3566d819
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-23 11:58:29 +0900

    練習5-3(練習5-2に追記したのみ/更新部分が出力される)

--- Main5.java
+++ Main5.java
@@ -164,4 +164,25 @@ public class Main {
 	}
 
 
+	// 練習5-3(練習5-2に追記したのみ/更新部分が出力される)
+	public static void main(String[] args) {
+	    String title = "お誘い";
+	    String address = "use@xxx.com";
+	    String text = "今度遊びませんか？";
+	    email(title, address, text);
+	}
+
+	public static void email(String address, String text) {
+	    System.out.println(address + "に、以下のメールを送信しました。");
+	    System.out.println("件名：無題");
+	    System.out.println("本文：" + text);
+	}
+
+	public static void email(String title, String address, String text) {
+	    System.out.println(address + "に、以下のメールを送信しました。");
+	    System.out.println("件名：" + title);
+	    System.out.println("本文：" + text);
+	}
+
+
 }
