7fe75d021520191dac801e422b1b2c5c0e091db1
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-27 17:48:58 +0900

    import文と完全限定クラス比較

--- Calc.java
+++ Calc.java
@@ -1,11 +1,13 @@
 // 第6章 複数クラスを用いた開発　〜コード6_4まで
 // package化する場合、下一文追加
 package calcapp.main;
+import calcapp.logics.CalcLogic;
+
 public class Calc {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
 		int total = CalcLogic.tasu(a, b);//CalcLogic.javaのtasuメソッドを呼び出す
-		int delta = CalcLogic.hiku(a, b);
+		int delta = calcapp.logics.CalcLogic.hiku(a, b);//package指定の場合は前につける
 		System.out.println("足すと" + total +"、引くと" + delta);
 	}
 
