01ef4c67e20046cc49bc475c8094f93e4d63e5d4
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-27 17:37:12 +0900

    差分更新

--- Calc.java
+++ Calc.java
@@ -1,17 +1,18 @@
-// 第6章 複数クラスを用いた開発
+// 第6章 複数クラスを用いた開発　〜コード6_4まで
 public class Calc {
 	public static void main(String[] args) {
 		int a = 10; int b = 2;
-		int total = tasu(a, b);
-		int delta = hiku(a, b);
+		int total = CalcLogic.tasu(a, b);//CalcLogic.javaのtasuメソッドを呼び出す
+		int delta = CalcLogic.hiku(a, b);
 		System.out.println("足すと" + total +"、引くと" + delta);
 	}
 
-	public static int tasu(int a, int b) {
-	    return (a + b);
-	}
+	// 以下の計算メソッドは、CalcLogic.javaへ移動
+	// public static int tasu(int a, int b) {
+	//     return (a + b);
+	// }
 
-	public static int hiku(int a, int b) {
-	    return (a - b);
-	}
+	// public static int hiku(int a, int b) {
+	//     return (a - b);
+	// }
 }
---
+++ CalcLogic.java
@@ -0,0 +1,10 @@
+// 計算処理のメソッドを分離させ、CalcLogic.javaへ計算処理に追加する。
+public class CalcLogic {
+	public static int tasu(int a, int b) {
+		return (a + b);
+	}
+
+	public static int hiku(int a, int b) {
+		return (a - b);
+	}
+}
