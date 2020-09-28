b76039c2ba63c743c7eaf6f67ae410978e139e2d
Tatsuro Muto <tomuto0221@gmail.com>
2020-09-28 15:37:22 +0900

    属性を宣言するにはクラスブロックの中に変数宣言

---
+++ Hero.java
@@ -0,0 +1,12 @@
+// コード8-2 HeroクラスをJavaのコードで表す
+public class Hero {
+	// 属性を宣言するにはクラスブロックの中に変数宣言の記述をする
+	String name;//名前の宣言
+	int hp;//HPの宣言
+
+	public void attack() {...}
+	public void sleep()
+	public void sit (int sec)
+	public void slip()
+	public void run()
+}
