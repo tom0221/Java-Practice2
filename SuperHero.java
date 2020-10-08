4570600fa1a91c8ecb025b49b854d88fd4060e9b
Tatsuro Muto <tomuto0221@gmail.com>
2020-10-08 11:55:38 +0900

    // コード10-9 親クラス(Heroクラス)のattackを呼び出す if文活用

--- SuperHero.java
+++ SuperHero.java
@@ -2,6 +2,7 @@
 public class SuperHero extends Hero { //extendが基本的にはHeroと同じという意味！
 	boolean flying; // flying追加
 
+
 	// ①追加したfly()
 	public void fly() {
 		this.flying = true;
@@ -17,6 +18,15 @@ public class SuperHero extends Hero { //extendが基本的にはHeroと同じと
 	// コード10-5 SuperHeroクラスにrun()を再定義する(上書きの扱い、オーバーライド)
 	public void run() {
 		System.out.println(this.name + "は撤退した！"); //逃げ出したから撤退したに上書き
+	}
+
+	// コード10-9 親クラス(Heroクラス)のattackを呼び出す if文活用
+	public void attack(Matango m) {
+		super.attack(m); // 親インスタンスのattackを呼び出し
+		if(this.flying) {
+			super.attack(m); // 親インスタンスのattackを呼び出し
+		}
+
 	}
 
 }
