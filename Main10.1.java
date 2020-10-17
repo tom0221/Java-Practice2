e69c5f8643b53b19b80c63ea215f2381fc8a8a34
Tatsuro Muto <tomuto0221@gmail.com>
2020-10-17 19:17:17 +0900

    重複

--- Main10.1.java
+++ Main10.1.java
@@ -4,3 +4,11 @@ public class Main {
 		Weapon w = new Weapon();
 	}
 }
+
+
+// コード10-11 コンストラクタを呼び出せない状況
+public class Main {
+	public static void main(String[] args) {
+		Weapon w = new Weapon();
+	}
+}
