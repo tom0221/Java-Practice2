dd2baca2225cc610e286ccaae9bdc7eb78ffe532
Tatsuro Muto <tomuto0221@gmail.com>
2020-10-28 16:22:12 +0900

    2.hフィールドに初期値をセット

--- Main8.2.java
+++ Main8.2.java
@@ -2,6 +2,13 @@
 public class Main {
 	public static void main(String[] args) {
 		// ここへ勇者への指示を書いていく
-		
+
+		// 1.勇者を生成 (Heroクラスからインスタンスを生成し、変数hに入れる)
+		Hero h = new Hero();
+
+		// 2.hフィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
 	}
 }
