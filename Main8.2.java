e1414bc33068fcaac77ee35bd7b0afa99568e623
Tatsuro Muto <tomuto0221@gmail.com>
2020-11-05 19:14:32 +0900

    Heroクラスからインスタンスを生成

--- Main8.2.java
+++ Main8.2.java
@@ -1,7 +1,10 @@
-// コード7-3
 // コード8-1 mainメソッドを作る
 public class Main {
-	
+	public static void main(String[] args) {
+		// ここへ勇者への指示を書いていく
 
+		// 1.勇者を生成 (Heroクラスからインスタンスを生成し、変数hに入れる)
+		Hero h = new Hero();
 
+	}
 }
