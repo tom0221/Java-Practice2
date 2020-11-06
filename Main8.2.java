3be981e7784d9f2952e2a89a8438b669fc260dc7
Tatsuro Muto <tomuto0221@gmail.com>
2020-11-06 17:53:55 +0900

    ソッドを順に呼び出していくとコメント追加

--- Main8.2.java
+++ Main8.2.java
@@ -6,5 +6,16 @@ public class Main {
 		// 1.勇者を生成 (Heroクラスからインスタンスを生成し、変数hに入れる)
 		Hero h = new Hero();
 
+		// 2.hフィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
+
+		// 3.勇者のメソッドを順に呼び出していく
+		h.sit(5);//5秒座れ
+		h.slip();//転べ
+		h.sit(25);//25秒座れ
+		h.run();//逃げろ
+
 	}
 }
