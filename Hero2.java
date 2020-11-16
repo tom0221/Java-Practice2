eea91607058aa59e2a6517da3c657541623c2436
Tatsuro Muto <tomuto0221@gmail.com>
2020-11-16 12:01:13 +0900

    操作と属性それぞれ定義

---
+++ Hero2.java
@@ -0,0 +1,13 @@
+// Heroクラス(2回目)
+public class Hero {
+	// 属性定義
+	String name;
+	int hp;
+
+	// 操作定義
+	public void attack()
+	public void sleep()
+	public void sit(int sec)
+	public void slip()
+	public void run()
+}
--- Main8.3.java
+++ Main8.3.java
@@ -1,1 +1,6 @@
 // コード8-1 mainメソッド作成
+public class Main {
+	public void static main[] args {
+		
+	}
+}
