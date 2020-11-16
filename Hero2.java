9190461c8644fe7b5bb4fd6f457f90fc9548ffb2
Tatsuro Muto <tomuto0221@gmail.com>
2020-11-16 12:16:21 +0900

    sleep〜runメソッドまでの記述

--- Hero2.java
+++ Hero2.java
@@ -10,10 +10,26 @@ public class Hero {
 		System.out.println(this.name + "は、眠って回復した！");
 	}
 
+	public void sit(int sec) {
+		this.hp += sec;
+		System.out.println(this.name + "は" + sec + "秒座った！");
+		System.out.println("HPが" + sec + "ポイント回復した！");
+	}
+
+	public void slip() {
+		this.hp -= 5;
+		System.out.println(this.name + "は、転んだ！");
+		System.out.println("5のダメージ！");
+	}
+
+	public void run() {
+		System.out.println(this.name + "は、逃げ出した！");
+		System.out.println("GAME OVER");
+		System.out.println("最終HPは" + this.hp + "でした");
+	}
 
 
-	public void attack()
-	public void sit(int sec)
-	public void slip()
-	public void run()
+
+	// public void attack()
+
 }
