be88259b467987489768c02d6b5e0bc428a1ff74
Tatsuro Muto <tomuto0221@gmail.com>
2020-11-17 09:29:47 +0900

    // 3.勇者のメソッドを呼び出す

--- Main8.3.java
+++ Main8.3.java
@@ -1,6 +1,27 @@
 // コード8-1 mainメソッド作成
 public class Main {
 	public static void main(String[] args) {
+		// 1.勇者を生成(インスタンス化)
+		Hero h = new Hero();
+		// 2.フィールドに初期値をセット
+		h.name = "ミナト";
+		h.hp = 100;
+		System.out.println("勇者" + h.name + "を生み出しました！");
 
+		// 1匹目のお化けキノコを生成する
+		Matango m1 = new Matabgo();
+		m1.hp = 50;
+		m1.suffix = 'A';
+
+		// 2匹目のお化けキノコを生成する
+		Matango m2 = new Matango();
+		m2.hp = 28;
+		m2.suffix = 'B';
+
+		// 3.勇者のメソッドを呼び出す
+		h.sit(5);
+		h.slip();
+		h.sit(25);
+		h.run();
 	}
 }
--- Matango2.java
+++ Matango2.java
@@ -2,4 +2,8 @@
 public class Matango {
 	int hp;
 	final int LEVEL = 10; // 初期値を10に設定する+finalをつけ定数として宣言する
+	char suffix;
+	public void run() {
+		System.out.println("お化けキノコ" + this.suffix + "は逃げ出した！");
+	}
 }
